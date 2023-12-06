20acf5f31c84f3297162fc3f9ecd747f8b95db68
Eileen-10 <93325000+Eileen-10@users.noreply.github.com>
2023-12-07 02:43:07 +0800

    added rotation

--- gasket4.js
+++ gasket4.js
@@ -7,6 +7,10 @@ var colors = [];
 var initialScale = 1;
 var NumTimesToSubdivide = 3;
 
+var axis = 0; //(x=0, y=1, z=2)
+var theta = [0, 0, 0];
+var thetaLoc;
+
 window.onload = function init() {
     canvas = document.getElementById("gl-canvas");
 
@@ -15,14 +19,51 @@ window.onload = function init() {
         alert("WebGL isn't available");
     }
 
-    //
-    //  Initialize our data for the Sierpinski Gasket
-    //
+    createGasket();
 
-    // First, initialize the vertices of our 3D gasket
-    // Four vertices on unit circle
-    // Intial tetrahedron with equal length sides
+    //  Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
+    // Enable hidden-surface removal
+    gl.enable(gl.DEPTH_TEST);
+
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Create a buffer object, initialize it, and associate it with the
+    //  associated attribute variable in our vertex shader
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    thetaLoc = gl.getUniformLocation(program, "theta"); 
+
+    axis = 2;   // z-axis
+
+    render();
+};
+
+//
+//  Initialize our data for the Sierpinski Gasket
+//
+// First, initialize the vertices of our 3D gasket
+// Four vertices on unit circle
+// Intial tetrahedron with equal length sides
+function createGasket(){
     var vertices = [
         vec3(0.0, 0.0, -1.0),
         vec3(0.0, 0.9428, 0.3333),
@@ -42,43 +83,7 @@ window.onload = function init() {
         vertices[3],
         NumTimesToSubdivide
     );
-
-    //
-    //  Configure WebGL
-    //
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
-
-    // enable hidden-surface removal
-
-    gl.enable(gl.DEPTH_TEST);
-
-    //  Load shaders and initialize attribute buffers
-
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
-
-    // Create a buffer object, initialize it, and associate it with the
-    //  associated attribute variable in our vertex shader
-
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
-
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    render();
-};
+}
 
 function triangle(a, b, c, color) {
     // add colors and vertices for one triangle
@@ -136,6 +141,12 @@ function divideTetra(a, b, c, d, count) {
 
 function render() {
     gl.clearColor(0.5, 0.5, 0.5, 1.0); // Set the background color to gray
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);    // Clear color and depth buffer
+    
+    theta[axis] += 2.0;
+    gl.uniform3fv(thetaLoc, theta);
+    
     gl.drawArrays(gl.TRIANGLES, 0, points.length);
+
+    requestAnimFrame(render);
 }
